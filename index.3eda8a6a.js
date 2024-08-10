79861e169afea2ef22f44a833e6ae1fb7377035a
Denys209818 <deniskravcuk63@gmail.com>
2024-08-10 13:35:54 +0300

    make build

---
+++ index.3eda8a6a.js
@@ -0,0 +1,2 @@
+var t=document.getElementsByClassName("spider")[0];t.style.position="absolute",t.style.top="50%",t.style.left="50%",t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.3eda8a6a.js.map
